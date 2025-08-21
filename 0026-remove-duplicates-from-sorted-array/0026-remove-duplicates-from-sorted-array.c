4618b4def0197900f8691ce1883282328195f76e
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2025-08-21 19:32:04 +0530

    Time: 3 ms (13.1%), Space: 12.2 MB (62.09%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,13 @@
+int removeDuplicates(int* nums, int numsSize)
+{
+   int index = 1;
+   for (int i = 1; i < numsSize; i++)
+   {
+        if (nums[i] != nums[i-1])
+        {
+            nums[index] = nums[i];
+            index++;
+        }
+   }
+   return index;
+}
