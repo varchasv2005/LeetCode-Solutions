7563988fb03acbed62a7cba9b55950b8d8fe4a9b
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2025-08-28 01:03:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (81.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x)
+{
+    if (x < 0) return false;
+    long reversed = 0;
+    int original = x;
+    while (x != 0)
+    {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+    return original == reversed;
+}
