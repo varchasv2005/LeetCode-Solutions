925399d221e38995cfb13b1b012b6931ac089481
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-06-02 22:51:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.86%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,14 +1,21 @@
-class Solution
-{
-    public boolean isPerfectSquare(int num)
-    {
-        for (long i = 1; i * i <= num; i++)
-        {
-            if (i * i == num)
-            {
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long left = 1;
+        long right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
                 return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+
         return false;
     }
 }
