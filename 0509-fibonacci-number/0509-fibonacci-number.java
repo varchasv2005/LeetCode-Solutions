2bb72581e9313023f38db2260704a52794d90a6e
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-06-05 21:57:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -16,11 +16,11 @@ class Solution
         arr[0] = 0;
         arr[1] = 1;
 
-        for (int i = 2; i < n; i++)
+        for (int i = 2; i < n+1; i++)
         {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
 
-        return arr[n-1] + arr[n-2];
+        return arr[n];
     }
 }
