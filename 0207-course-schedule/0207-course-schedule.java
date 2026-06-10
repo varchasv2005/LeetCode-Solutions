c8c5c61cf88f82c44ca195f3729ab453f5c5f22d
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-06-10 22:07:52 +0530

    Time: 6 ms (72.99%), Space: 46.9 MB (86.73%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        int[] indegree = new int[numCourses];
+        
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int completed = 0;
+        
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            completed++;
+            
+            for (int neighbor : graph.get(course)) {
+                indegree[neighbor]--;
+                
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        return completed == numCourses;
+    }
+}
