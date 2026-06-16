fa0a4735e27b4e2ff7438dce02696bcc146a4c5e
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-06-17 02:27:01 +0530

    Time: 3 ms (67.81%), Space: 46.5 MB (51.47%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSame(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || 
+               isSubtree(root.right, subRoot);
+    }
+    private boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSame(p.left, q.left) && 
+               isSame(p.right, q.right);
+    }
+}
