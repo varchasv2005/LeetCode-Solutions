66d997dbf3a8759bd32fbd70880d384ec005864e
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-06-25 03:02:34 +0530

    Time: 4 ms (98.76%), Space: 46 MB (41.34%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') y++;
+            else if (c == 'D') y--;
+            else if (c == 'L') x--;
+            else x++;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
