b86b1a5cf62225851d088739ceffecca41008806
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-07-03 00:12:06 +0530

    Time: 0 ms (100%), Space: 44.5 MB (29.63%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (String op : operations) {
+            if (op.charAt(1) == '+')
+                x++;
+            else
+                x--;
+        }
+
+        return x;
+    }
+}
