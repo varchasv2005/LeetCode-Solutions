d50685f9741f80af4eb67b98ea1fc941f4e56c97
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-07-13 22:51:30 +0530

    Time: 5 ms (61%), Space: 44.9 MB (47.8%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            sb.append(c);
-
-            int n = sb.length();
-
-            if (n >= 2) {
-                String last = sb.substring(n - 2);
-
-                if (last.equals("AB") || last.equals("CD"))
-                    sb.delete(n - 2, n);
-            }
+            if (!stack.isEmpty() &&
+               ((stack.peek() == 'A' && c == 'B') ||
+                (stack.peek() == 'C' && c == 'D')))
+                stack.pop();
+            else
+                stack.push(c);
         }
 
-        return sb.length();
+        return stack.size();
     }
 }
