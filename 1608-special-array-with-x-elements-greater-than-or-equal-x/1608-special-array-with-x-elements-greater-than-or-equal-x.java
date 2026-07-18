dfd99dfecad7240564fdcf8dd1cb32937a9b86fc
Varchasv <149260994+varchasv2005@users.noreply.github.com>
2026-07-19 00:02:11 +0530

    Time: 5 ms (20.23%), Space: 43.6 MB (9.49%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int x = n - i;
+            if (nums[i] >= x && (i == 0 || nums[i - 1] < x))
+                return x;
+        }
+
+        return -1;
+    }
+}
